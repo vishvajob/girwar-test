1e1e4ad4ecb3db20d66337b37cedd58b8c1c7e6c
Vishvanath Surwshe <vsurwshe@gmail.com>
2021-01-09 10:25:37 +0530

    0.0.5: implemented top header component

--- src/component/MainCompoent.js
+++ src/component/MainCompoent.js
@@ -3,10 +3,12 @@ import DeatilsComponent from './DetailsCompoent/DetailsComponent';
 import JumbotronEelement from './Jumbotron/JumbotronElement';
 import "../assest/style.css"
 import AboutUs from './AboutUs/AboutUs';
+import TopHeader from './TopHeader/TopHeader';
 class MainCompoent extends Component {
     state = {  }
     render() { 
         return <div className="mainBody">
+            <TopHeader />
             <JumbotronEelement />
             <DeatilsComponent />
             <AboutUs />
---
+++ src/component/TopHeader/TopHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../../assest/style.css"
+
+const TopHeader=(propsData)=>{
+    return<div className="breadcrumbTop">
+        <img src="image/circle.png" style={{width:"25px"}}/>&nbsp;&nbsp;
+        |&nbsp;&nbsp;Healthy Fruits
+    </div>    
+}
+
+export default TopHeader;
