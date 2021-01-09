e3ff9ea27aa38061b4d771caa555ee60ea684201
Vishvanath Surwshe <vsurwshe@gmail.com>
2021-01-09 07:24:39 +0530

    0.0.1: Initialize the code

---
+++ src/component/MainCompoent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MainCompoent extends Component {
+    state = {  }
+    render() { 
+        return <h2>Main Compoent</h2>;
+    }
+}
+ 
+export default MainCompoent;
